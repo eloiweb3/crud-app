3c88f73988cb10ea02ce6222f7b364a2038d0799
Eloi <es@gsmk.de>
2024-11-08 13:32:25 +0100

    create Journal

--- anchor/programs/crudapp/src/lib.rs
+++ anchor/programs/crudapp/src/lib.rs
@@ -6,7 +6,43 @@ declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
 #[program]
 pub mod crudapp {
+
     use super::*;
 
+    pub fn create_journal_entry(ctx: Context<CreateJournalEntry>, title: String, message: String) -> Result<()> {
+            let journal_entry = &mut ctx.accounts.journal_entry;
+            journal_entry.owner = *ctx.accounts.owner.key;
+            journal_entry.title = title;
+            journal_entry.message = message;
+            Ok(())
 
+    }
+
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct CreateJournalEntry<'info> {
+    #[account(
+        init,
+        seeds= [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        space= 8 + JournalEntryState::INIT_SPACE,
+        payer = owner,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState {
+    pub owner: Pubkey,
+    #[max_len(50)]
+    pub title: String,
+    #[max_len(1000)]
+    pub message: String
 }
