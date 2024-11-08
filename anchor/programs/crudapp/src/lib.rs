fe67e07b3e4056e39f4a1777026ca33781a987d3
Eloi <es@gsmk.de>
2024-11-08 14:43:39 +0100

    delete Journal instruction

--- anchor/programs/crudapp/src/lib.rs
+++ anchor/programs/crudapp/src/lib.rs
@@ -18,6 +18,16 @@ pub mod crudapp {
 
     }
 
+    pub fn update_journal_entry(ctx: Context<UpdateJournalEntry>, _title: String, message: String) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.message = message;
+        Ok(())
+    }
+
+    pub fn delete_journal_entry(_ctx: Context<DeleteJournalEntry>, _title: String) -> Result<()> {
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
@@ -25,9 +35,9 @@ pub mod crudapp {
 pub struct CreateJournalEntry<'info> {
     #[account(
         init,
-        seeds= [title.as_bytes(), owner.key().as_ref()],
+        seeds = [title.as_bytes(), owner.key().as_ref()],
         bump,
-        space= 8 + JournalEntryState::INIT_SPACE,
+        space = 8 + JournalEntryState::INIT_SPACE,
         payer = owner,
     )]
     pub journal_entry: Account<'info, JournalEntryState>,
@@ -35,6 +45,43 @@ pub struct CreateJournalEntry<'info> {
     pub owner: Signer<'info>,
     pub system_program: Program<'info, System>
 }
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct UpdateJournalEntry<'info> {
+    #[account(
+        mut,
+        seeds= [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        realloc = 8 + JournalEntryState::INIT_SPACE,
+        realloc::payer = owner,
+        realloc::zero = true
+    )]
+
+    pub journal_entry: Account<'info, JournalEntryState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+}
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteJournalEntry<'info> {
+    #[account(
+        mut,
+        seeds= [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        close = owner
+    )]
+
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+
+
 
 
 #[account]
